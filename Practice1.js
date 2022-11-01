54622ba638b6f122c0ee7fde4f3378ba157e2bbd
Nilesh <nileshsutar141977@gmail.com>
2022-11-01 21:13:25 +0530

    first commit

---
+++ Practice1.js
@@ -0,0 +1,16 @@
+// let names = "Main Kon Hoon";
+// console.log(`${names}`, "--->Main Hoon Don");
+
+// let num = 254;
+
+// let tax = "GST";
+// let Numbers = 2454;
+// const carName = {
+//   color: "black",
+//   company: "Scoda",
+//   Numbers: 2454,
+// };
+
+// console.log(typeof num, typeof tax, typeof Numbers);
+
+// // console.log("I am Good Boy");
